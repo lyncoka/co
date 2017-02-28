3a9dd904a0679c21b84d67700185cad017165cb4
pc <pancheng@tospur.com>
2017-02-28 16:14:46 +0800

    commit

---
+++ cokaka/app/src/main/java/com/example/a37616/cokaka/kakaActivity.java
@@ -0,0 +1,21 @@
+package com.example.a37616.cokaka;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class kakaActivity extends AppCompatActivity {
+
+    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_kaka);
+//    }
+
+    protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_kaka);
+//            TextView tv1 = (TextView)this.findViewById(R.id.textView1);
+//            tv1.setText("dage");
+         }
+}
+
